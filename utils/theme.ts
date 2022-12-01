dfad57d790a6cd337c8b8a8cf14c50c46508f883
Tommy Wong <tommywong.dev@gmail.com>
2022-12-01 18:37:53 +0800

    added brand color

--- utils/theme.ts
+++ utils/theme.ts
@@ -2,9 +2,16 @@ import { ChakraTheme, extendTheme } from "@chakra-ui/react";
 
 const colors: ChakraTheme["colors"] = {
   brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
+    50: "#e3f7fd",
+    100: "#b9ebfa",
+    200: "#8ddef6",
+    300: "#65d1ef",
+    400: "#50c7e8",
+    500: "#4abde1",
+    600: "#44adcd",
+    700: "#3b99b2",
+    800: "#348599",
+    900: "#25626d",
   },
 };
 
