25702fbbf72f09ef87d54ba047db6dfdca8e455d
Tommy Wong <tommywong.dev@gmail.com>
2022-12-03 21:10:12 +0800

    pass typescript object

--- utils/trimObjStrings.ts
+++ utils/trimObjStrings.ts
@@ -1,12 +1,12 @@
-export const trimObjStrings = <T>(target: T): T => {
+export const trimObjStrings = (target: object): object => {
   if (typeof target !== "object") return target;
 
   return Object.keys(target).reduce((prev, curr) => {
-    const val = target[curr as keyof T];
+    const val = target[curr as keyof object];
 
     return {
       ...prev,
-      [curr]: typeof val === "string" ? val.trim() : val,
+      [curr]: typeof val === "string" ? (val as string).trim() : val,
     };
-  }, {} as T);
+  }, {});
 };
