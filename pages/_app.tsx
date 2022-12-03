47d19d59de23b778eea06a804d5dd989ef6eb720
Tommy Wong <tommywong.dev@gmail.com>
2022-12-03 21:37:39 +0800

    added app title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { theme } from "../theme";
 import { AlertProvider } from "../providers";
+import Head from "next/head";
+import { useTranslations } from "../hooks";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const t = useTranslations();
+
   return (
     <ChakraProvider theme={theme}>
       <AlertProvider>
+        <Head>
+          <title>{t.appTitle}</title>
+        </Head>
         <Component {...pageProps} />
       </AlertProvider>
     </ChakraProvider>
